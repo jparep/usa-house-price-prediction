29f72ac557619ded09e32cdd6873b2fdfcf6c219
jparep <parepson@gmail.com>
2024-08-17 17:24:23 -0600

    Scalled features

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ df = pd.read_csv('./data/USA_Housing.csv')
 # Feature Engineering: Adding Room-to-Bedroom Ration as a new features
 df['Room2Bedroom_ratio'] = df['Avg. Area Number of Rooms'] / df['Avg. Area Number of Bedrooms']
 
+# Define the feature variables and target variable
+X = df.drop(['Price', 'Address'])
+y = df['Price']
+
+# Scale Features
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
