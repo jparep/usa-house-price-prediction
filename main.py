c26fbe760ad0170dee02816c26c5efcc20a9af87
jparep <parepson@gmail.com>
2024-08-16 22:27:45 -0600

    Evaluate model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Load Data
 df = pd.read_csv('./data/USA_Housing.csv')
@@ -25,4 +26,11 @@ lr.fit(X_train, y_train)
 cdf = pd.DataFrame(lr.coef_, X.columns, columns=['Coeff'])
 print(cdf)
 
+y_pred = lr.predict(X_test)
+
+plt.scatter(y_test, y_pred)
+
+mean_absolute_error(y_test, y_pred)
+
+mean_squared_error(y_test, y_pred)
 
