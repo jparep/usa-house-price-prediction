af0b4159fc4fe759726ef8c894e35cefdb551292
jparep <parepson@gmail.com>
2024-08-17 17:21:42 -0600

    Created rom to bedroom ratio

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+# Load the housing data into DataFrame
+df = pd.read_csv('./data/USA_Housing.csv')
+
+# Feature Engineering: Adding Room-to-Bedroom Ration as a new features
+df['Room2Bedroom_ratio'] = df['Avg. Area Number of Rooms'] / df['Avg. Area Number of Bedrooms']
+
