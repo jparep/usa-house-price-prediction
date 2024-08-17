63e2a8076def6bb3ba38d3962b01379511e07762
jparep <parepson@gmail.com>
2024-08-16 22:49:37 -0600

    Initalize the Linear Regression Model

--- main.py
+++ main.py
@@ -2,35 +2,54 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-# Load Data
+# Load the housing data into a DataFrame
 df = pd.read_csv('./data/USA_Housing.csv')
 
-# Visialize data
+# Visualize the relationship between features with pair plots
 sns.pairplot(df)
 
+# Visualize the distribution of the target variable
 sns.distplot(df['Price'])
 
+# Define the feature variables (X) and target variable (y)
+# Dropping 'Price' as its tje target variable, and 'Address' as the it's irrelevant variable
 X = df.drop(['Price', 'Address'], axis=1)
-
 y = df['Price']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+# Split the data into training and testing sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(X,y, train_size=0.2, random_state=123)
 
+# Initialize the Linear Regression Model
 lr = LinearRegression()
+
+# Train the model using the training data
 lr.fit(X_train, y_train)
 
+# Create a DataFrame to view hthe coefficients of the model
 cdf = pd.DataFrame(lr.coef_, X.columns, columns=['Coeff'])
 print(cdf)
 
+# Predict the target values (House prices) using the test data
+
 y_pred = lr.predict(X_test)
 
+# Plot the actual vs predicted values to vizualize model performance
 plt.scatter(y_test, y_pred)
+plt.xlabel('Actual Prices')
+plt.ylabel('Predicted Prices')
+plt.title('Actual vs Predicted House Prices')
 
-mean_absolute_error(y_test, y_pred)
+# Show plots
+plt.show
 
-mean_squared_error(y_test, y_pred)
+# Evaluate the model using mean absolute error(MAE)
+mae = mean_absolute_error(y_test, y_pred)
+print(f'Mean Absolute Error: {mae}')
 
+# Evaluate the mdoel using mean squared error (MSE)
+mse = mean_squared_error(y_test, y_pred)
+print('Mean Squared Error: {mse}')
