543eb59fc3f5dab1ae8850672a75aa85e8da477e
jparep <parepson@gmail.com>
2024-08-19 22:26:14 -0600

    Optimizer Handle outlier method to use quantile approach

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ def load_data(file_path):
         raise e
     except Exception as e:
         logging.error(f"An error occured while loading the data: {e}")
+
+def handle_outliers(df, columns):
+    """Handle oultiers using the 1st quartile and 3rd quartile"""
+    for col in columns:
+        if col in df.columns:
+            Q1 = df[col].quantile(0.25)
+            Q3 = df[col].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+            df[col] = np.clip(df[col], lower_bound, upper_bound)
+    return df
